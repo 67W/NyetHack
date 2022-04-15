b983e955a80c7caedc41c84a0d3c42284ea0d15b
67W <error4434@gmail.com>
2022-04-15 23:30:32 +0300

    Оператор ?: Листинг 6.10. Использование оператора ?: (Tavern.kt)

--- src/main/kotlin/tavern.kt
+++ src/main/kotlin/tavern.kt
@@ -1,11 +1,11 @@
 fun main() {
-    var beverage = readLine()?.let {
-        if (it.isNotBlank()) {
-            it.capitalize()
-        } else {
-            "Buttered Ale"
-        }
+    var beverage = readLine()
+// beverage = null
+    if (beverage != null) {
+        beverage = beverage.capitalize()
+    } else {
+        println("I can't do that without crashing - beverage was null!")
     }
-//    beverage = null
-    println(beverage)
+    val beverageServed: String = beverage ?: "Buttered Ale"
+    println(beverageServed)
 }
